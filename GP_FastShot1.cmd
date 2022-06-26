e0bf68e2c5de5a4c1e89423ceb20fdabaea2b775
vovetskyy <vovetskyy@gmail.com>
2022-06-26 08:44:57 +0200

    Minor changes

--- GP_FastShot1.cmd
+++ GP_FastShot1.cmd
@@ -4,6 +4,7 @@ rem 1st parameter -- number of measurement loops (default value see in main.py)
 
 
 set CUR_PATH=%cd%
-echo %CUR_PATH%
+
+echo on
 
 %CUR_PATH%\venv\Scripts\python.exe %CUR_PATH%\main.py 4 > %CUR_PATH%\RawOutput\Test1.txt
