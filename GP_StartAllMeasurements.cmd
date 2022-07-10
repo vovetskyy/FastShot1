ad7fac27fbe046e755f0518e07986ce8ab511702
vovetskyy <vovetskyy@gmail.com>
2022-07-10 14:15:06 +0200

    - added number of measurement loops as parameter 1

--- GP_StartAllMeasurements.cmd
+++ GP_StartAllMeasurements.cmd
@@ -1,4 +1,9 @@
 echo off
+rem
+rem 1st parameter -- number of measurement loops (default value -- %DEF_NUM_LOOPS%)
+set NUM_LOOPS="%1"
+set DEF_NUM_LOOPS=5
+if %NUM_LOOPS%=="" set NUM_LOOPS=%DEF_NUM_LOOPS%
 
 set CUR_PATH=%cd%
 set PC_NAME=%COMPUTERNAME%
@@ -10,4 +15,4 @@ set SCRIPT1_OUT="%CUR_PATH%\RawOutput\%PC_NAME%_Script1.txt"
 echo on
 
 rem %INTEL_PG_PATH%\PowerLog3.0.exe -file %INTEL_PG_CSV_OUT% -duration 2
-%INTEL_PG_PATH%\PowerLog3.0.exe -file %INTEL_PG_CSV_OUT% -resolution 500 -cmd "GP_FastShot1.cmd %SCRIPT1_OUT% 2"
+%INTEL_PG_PATH%\PowerLog3.0.exe -file %INTEL_PG_CSV_OUT% -resolution 500 -cmd "GP_FastShot1.cmd %SCRIPT1_OUT% %NUM_LOOPS%"
