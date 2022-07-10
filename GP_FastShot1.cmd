028629c2c9e7643ee60619a3196219649be70752
vovetskyy <vovetskyy@gmail.com>
2022-07-10 14:05:58 +0200

    - added output file as parameter 1  - added num of loops as parameter 2

--- GP_FastShot1.cmd
+++ GP_FastShot1.cmd
@@ -1,12 +1,15 @@
 echo off
 rem 
-rem 1st parameter -- number of measurement loops (default value see in main.py)
+rem 1st parameter -- name of the output file
+rem 2nd parameter -- number of measurement loops (default value see in main.py)
 
+set NORM_OUT="%1"
+set NUM_LOOPs="%2"
 
 set CUR_PATH=%cd%
 set PC_NAME=%COMPUTERNAME%
 
-set NORM_OUT=%CUR_PATH%\RawOutput\%PC_NAME%_Test1.txt
+if %NORM_OUT%=="" set NORM_OUT=%CUR_PATH%\RawOutput\%PC_NAME%_Test1.txt
 
 rem %CUR_PATH%\venv\Scripts\python.exe %CUR_PATH%\main.py 4 > %CUR_PATH%\RawOutput\Test1.txt 2>error.log
-%CUR_PATH%\venv\Scripts\python.exe %CUR_PATH%\main.py 4 >%NORM_OUT% 2>error.log
+%CUR_PATH%\venv\Scripts\python.exe %CUR_PATH%\main.py %NUM_LOOPs% >%NORM_OUT% 2>error.log
