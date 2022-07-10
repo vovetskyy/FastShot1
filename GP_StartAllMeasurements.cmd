07082f74f7078f1b5557aa74331ea7cda613b05e
vovetskyy <vovetskyy@gmail.com>
2022-07-10 14:09:03 +0200

    - added name of output file for GP_FastShot1.cmd

--- GP_StartAllMeasurements.cmd
+++ GP_StartAllMeasurements.cmd
@@ -4,9 +4,10 @@ set CUR_PATH=%cd%
 set PC_NAME=%COMPUTERNAME%
 set INTEL_PG_PATH="c:\Program Files\Intel\Power Gadget 3.6"
 
-set INTEL_PG_CSV_OUT=%CUR_PATH%\RawOutput\%PC_NAME%_IPG.csv
+set INTEL_PG_CSV_OUT="%CUR_PATH%\RawOutput\%PC_NAME%_IPG.csv"
+set SCRIPT1_OUT="%CUR_PATH%\RawOutput\%PC_NAME%_Script1.txt"
 
 echo on
 
 rem %INTEL_PG_PATH%\PowerLog3.0.exe -file %INTEL_PG_CSV_OUT% -duration 2
-%INTEL_PG_PATH%\PowerLog3.0.exe -file %INTEL_PG_CSV_OUT% -resolution 500 -cmd GP_FastShot1.cmd
+%INTEL_PG_PATH%\PowerLog3.0.exe -file %INTEL_PG_CSV_OUT% -resolution 500 -cmd "GP_FastShot1.cmd %SCRIPT1_OUT% 2"
