4d8334efbfc45f51e3466fc79f9a5398d3495291
vovetskyy <vovetskyy@gmail.com>
2022-07-14 15:32:22 +0200

    - creation of the timestamp moved to GP_GetTimeStamp.cmd

--- GP_StartAllMeasurements.cmd
+++ GP_StartAllMeasurements.cmd
@@ -20,13 +20,7 @@ rem -----------------------------------------------------------------
 rem definition of ouptut names (including PC-name and timestamp)
 
 rem get timestamps
-for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
-set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
-set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
-
-set "DATE_TIMESTAMP=%YYYY%%MM%%DD%"
-set "TIME_TIMESTAMP=%HH%%Min%%Sec%"
-set "FULL_TIMESTAMP=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+call GP_GetTimeStamp.cmd
 rem now we have timestamp in the %FULL_TIMESTAMP%
 
 set PC_NAME=%COMPUTERNAME%
