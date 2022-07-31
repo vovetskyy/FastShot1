ba0340dea122fa96ff5621c6393cd477c6ece443
vovetskyy <vovetskyy@gmail.com>
2022-07-31 12:27:18 +0200

    - minor redactional changes

--- GP_FastShot1.cmd
+++ GP_FastShot1.cmd
@@ -1,15 +1,14 @@
-echo off
+echo on
 rem 
 rem 1st parameter -- name of the output file
 rem 2nd parameter -- number of measurement loops (default value see in main.py)
 
 set NORM_OUT="%1"
-set NUM_LOOPs="%2"
+set NUM_LOOPS="%2"
 
 set CUR_PATH=%cd%
 set PC_NAME=%COMPUTERNAME%
 
 if %NORM_OUT%=="" set NORM_OUT=%CUR_PATH%\RawOutput\%PC_NAME%_Test1.txt
 
-rem %CUR_PATH%\venv\Scripts\python.exe %CUR_PATH%\main.py 4 > %CUR_PATH%\RawOutput\Test1.txt 2>error.log
 %CUR_PATH%\venv\Scripts\python.exe %CUR_PATH%\main.py %NUM_LOOPs% >%NORM_OUT% 2>error.log
