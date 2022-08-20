ee2fffe369c0884e611d6ac4262e655a922b4d87
vovetskyy <vovetskyy@gmail.com>
2022-08-20 22:04:37 +0200

    - GP_FastShot1.py is called via GP_FastShot1.exe

--- GP_FastShot1_pyscript.cmd
+++ GP_FastShot1_pyscript.cmd
@@ -11,4 +11,4 @@ set PC_NAME=%COMPUTERNAME%
 
 if %NORM_OUT%=="" set NORM_OUT=%CUR_PATH%\RawOutput\%PC_NAME%_Test1.txt
 
-%CUR_PATH%\GP_FastShot1.exe %NUM_LOOPs% >%NORM_OUT% 2>error.log
+%CUR_PATH%\venv\Scripts\python.exe %CUR_PATH%\main.py %NUM_LOOPs% >%NORM_OUT% 2>error.log
